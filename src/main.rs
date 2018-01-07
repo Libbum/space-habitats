dd6192e6812edb89dd629806119008e578994b41
Tim DuBois <tim@neophilus.net>
2018-01-07 17:06:52 +0100

    Filled in some documentation

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,43 @@
+//! Space Habitiats: a sphere packing method to identify optimal layouts of deep space exploration habitats.
+//!
+//! Based on ideas discussed in:
+//!
+//! Valentina Sumini and Caitlin Mueller, Proceedings of the International Association for Shell and Spatial Structures (IASS) Symposium, 2017
+
 extern crate kiss3d;
 extern crate nalgebra;
 extern crate rand;
 
+/// Implementation of a modified version of the advancing front packing algorithm from
+/// Valera *et al.*, [Computational Particle Mechanics 2, 161 (2015)](https://doi.org/10.1007/s40571-015-0045-8).
+mod sphere_packing;
+
+use std::collections::VecDeque;
 use nalgebra::{Vector3, UnitQuaternion, Point3, Translation3};
 use rand::distributions::{IndependentSample, Range};
 use kiss3d::camera::ArcBall;
 use kiss3d::window::Window;
 use kiss3d::light::Light;
+use sphere_packing::{Sphere, pack_spheres};
 
 #[derive(Debug)]
+/// Sample room structure for testing. May be removed or significantly altered in the future.
 struct Room {
+    /// Name/function of the room in the habitat.
     name: String,
+    /// Room size, since it will be a sphere this is measured via its `radius`.
+    /// Test conditions are mostly less than one (for example, Sleep is 0.7) 
+    /// to fit within some camera value, but correspond to real values in meters.
+    /// Sleep in reality should be 700 m.
     radius: f32,
+    /// A color to distinguish the room in the visualisation.
     color: Color,
+    /// Location in space of current room.
     position: Point3<f32>,
 }
 
 impl Room {
+    /// Generates a `new` room based on user generated data.
     fn new(name: &str, radius: f32, color: Color, position: Point3<f32>) -> Room {
         Room {
             name: name.to_string(),
@@ -29,6 +50,7 @@ impl Room {
 
 
 #[derive(Debug)]
+/// Simple representation of an rgb color. Kiss3D requires values on $[0.0, 1.0]$.
 struct Color {
     red: f32,
     green: f32,
@@ -36,6 +58,7 @@ struct Color {
 }
 
 impl Color {
+    /// Constructs a `new` color based on rgb values between 0 and 1.
     fn new(red: f32, green: f32, blue: f32) -> Color {
         Color {
             red: red,
@@ -45,6 +68,7 @@ impl Color {
     }
 }
 
+/// Makes sure randomly added rooms to not lie inside or overlap with another room. Only needed for testing.
 fn find_disjoint_position(from: &Vec<Room>, distance: f32, between: &Range<f32>, rng: &mut rand::ThreadRng) -> Point3<f32> {
     let mut new_point = Point3::new(between.ind_sample(rng), between.ind_sample(rng), between.ind_sample(rng));
     loop {
@@ -56,6 +80,7 @@ fn find_disjoint_position(from: &Vec<Room>, distance: f32, between: &Range<f32>,
     }
 }
 
+/// Simple room constructor, currently hardcoded for testing and will most likely be removed in the future.
 fn construct_rooms() -> Vec<Room> {
     let between = Range::new(-1.5, 1.5);
     let mut rng = rand::thread_rng();
@@ -115,6 +140,7 @@ fn construct_rooms() -> Vec<Room> {
     rooms
 }
 
+/// Program entry point.
 fn main() {
 
     let habitat = construct_rooms();
